d359b44c140282a41dd16fb46b3b0ccf417ef472
itlgl <itlgl@outlook.com>
2018-03-23 15:58:05 +0800

    完善有关des的加解密和测试案例

--- src/main/java/com/itlgl/cryptoutil/des/DesUtil.java
+++ src/main/java/com/itlgl/cryptoutil/des/DesUtil.java
@@ -8,24 +8,14 @@ import java.util.Arrays;
 public class DesUtil {
     private static final DESEngine DES_ENGINE = new DESEngine();
 
-    /*
+    /**
      * des的加解密
      * key和src的长度必须是8字节
      * 返回的结果也是8字节
      */
-    protected static void desBlockFunc(byte[] key, byte[] src, int inOff, byte[] result, int outOff, boolean encrypting) {
+    protected static void desBlockFunc(final byte[] key, final byte[] src, final int inOff, final byte[] result, final int outOff, final boolean encrypting) {
         DES_ENGINE.init(encrypting, new KeyParameter(key));
         DES_ENGINE.processBlock(src, inOff, result, outOff);
-    }
-
-    private static final void xor(byte[] b1, byte[] b2, byte[] result, int length) throws DesException {
-        if(b1 == null || b2 == null || result == null ||
-                b1.length < length || b2.length < length || result.length < length) {
-            throw new DesException("param error");
-        }
-        for(int i = 0; i < length; i++) {
-            result[i] = (byte) ((b1[i] ^ b2[i]) & 0xff);
-        }
     }
 
     public static final byte[] desEcb(final byte[] key, final byte[] src, final boolean encrypting) throws DesException {
@@ -73,6 +63,146 @@ public class DesUtil {
             } else {
                 // 解密时，先解密，再异或
                 desBlockFunc(key, src, i * 8, block, 0, false);
+                for(int j = 0; j < 8; j++) {
+                    block[j] = (byte) ((block[j] ^ icvTem[j]) & 0xff);
+                }
+                System.arraycopy(block, 0, result, i * 8, 8);
+                // 将这一组的密文作为下一组解密后异或的icv
+                System.arraycopy(src, i * 8, icvTem, 0, 8);
+            }
+        }
+        return result;
+    }
+
+    public static final byte[] desEdeEcb(final byte[] key, final byte[] src, final boolean encrypting) throws DesException {
+        if(key == null || key.length != 24) {
+            throw new DesException("DesEDE key should be 24 bytes");
+        }
+        if(src == null || src.length == 0) {
+            throw new DesException("DesEDE src should not be empty");
+        }
+        if(src.length % 8 != 0) {
+            throw new DesException("DesEDE src length should be be an integer multiple of 8");
+        }
+        byte[] result = new byte[src.length];
+        byte[] key1 = Arrays.copyOfRange(key, 0, 8);
+        byte[] key2 = Arrays.copyOfRange(key, 8, 16);
+        byte[] key3 = Arrays.copyOfRange(key, 16, 24);
+        byte[] block1 = new byte[8], block2 = new byte[8];
+        for(int i = 0, len = src.length / 8; i < len; i++) {
+            desBlockFunc(key1, src, i * 8, block1, 0, encrypting);
+            desBlockFunc(key2, block1, 0, block2, 0, !encrypting);
+            desBlockFunc(key3, block2, 0, result, i * 8, encrypting);
+        }
+        return result;
+    }
+
+    public static final byte[] desEeeEcb(final byte[] key, final byte[] src, final boolean encrypting) throws DesException {
+        if(key == null || key.length != 24) {
+            throw new DesException("DesEDE key should be 24 bytes");
+        }
+        if(src == null || src.length == 0) {
+            throw new DesException("DesEDE src should not be empty");
+        }
+        if(src.length % 8 != 0) {
+            throw new DesException("DesEDE src length should be be an integer multiple of 8");
+        }
+        byte[] result = new byte[src.length];
+        byte[] key1 = Arrays.copyOfRange(key, 0, 8);
+        byte[] key2 = Arrays.copyOfRange(key, 8, 16);
+        byte[] key3 = Arrays.copyOfRange(key, 16, 24);
+        byte[] block1 = new byte[8], block2 = new byte[8];
+        for(int i = 0, len = src.length / 8; i < len; i++) {
+            desBlockFunc(key1, src, i * 8, block1, 0, encrypting);
+            desBlockFunc(key2, block1, 0, block2, 0, encrypting);
+            desBlockFunc(key3, block2, 0, result, i * 8, encrypting);
+        }
+        return result;
+    }
+
+    public static final byte[] desEdeCbc(final byte[] key, final byte[] src, final byte[] icv, final boolean encrypting) throws DesException {
+        if(key == null || key.length != 24) {
+            throw new DesException("DesEDE key should be 24 bytes");
+        }
+        if(icv == null || icv.length != 8) {
+            throw new DesException("DesEDE icv should be 8 bytes");
+        }
+        if(src == null || src.length == 0) {
+            throw new DesException("DesEDE src should not be empty");
+        }
+        if(src.length % 8 != 0) {
+            throw new DesException("DesEDE src length should be be an integer multiple of 8");
+        }
+        byte[] result = new byte[src.length];
+        byte[] key1 = Arrays.copyOfRange(key, 0, 8);
+        byte[] key2 = Arrays.copyOfRange(key, 8, 16);
+        byte[] key3 = Arrays.copyOfRange(key, 16, 24);
+        byte[] block = new byte[8];
+        byte[] icvTem = Arrays.copyOfRange(icv, 0, 8);
+        byte[] block1 = new byte[8], block2 = new byte[8];
+        for(int i = 0, len = src.length / 8; i < len; i++) {
+            if(encrypting) {
+                // 加密时，先异或，再加密
+                System.arraycopy(src, i * 8, block, 0, 8);
+                for(int j = 0; j < 8; j++) {
+                    block[j] = (byte) ((block[j] ^ icvTem[j]) & 0xff);
+                }
+                desBlockFunc(key1, block, 0, block1, 0, true);
+                desBlockFunc(key2, block1, 0, block2, 0, false);
+                desBlockFunc(key3, block2, 0, result, i * 8, true);
+                System.arraycopy(result, i * 8, icvTem, 0, 8);
+            } else {
+                // 解密时，先解密，再异或
+                desBlockFunc(key1, src, i * 8, block1, 0, false);
+                desBlockFunc(key2, block1, 0, block2, 0, true);
+                desBlockFunc(key3, block2, 0, block, 0, false);
+                for(int j = 0; j < 8; j++) {
+                    block[j] = (byte) ((block[j] ^ icvTem[j]) & 0xff);
+                }
+                System.arraycopy(block, 0, result, i * 8, 8);
+                // 将这一组的密文作为下一组解密后异或的icv
+                System.arraycopy(src, i * 8, icvTem, 0, 8);
+            }
+        }
+        return result;
+    }
+
+    public static final byte[] desEeeCbc(final byte[] key, final byte[] src, final byte[] icv, final boolean encrypting) throws DesException {
+        if(key == null || key.length != 24) {
+            throw new DesException("DesEEE key should be 24 bytes");
+        }
+        if(icv == null || icv.length != 8) {
+            throw new DesException("DesEEE icv should be 8 bytes");
+        }
+        if(src == null || src.length == 0) {
+            throw new DesException("DesEEE src should not be empty");
+        }
+        if(src.length % 8 != 0) {
+            throw new DesException("DesEEE src length should be be an integer multiple of 8");
+        }
+        byte[] result = new byte[src.length];
+        byte[] key1 = Arrays.copyOfRange(key, 0, 8);
+        byte[] key2 = Arrays.copyOfRange(key, 8, 16);
+        byte[] key3 = Arrays.copyOfRange(key, 16, 24);
+        byte[] block = new byte[8];
+        byte[] icvTem = Arrays.copyOfRange(icv, 0, 8);
+        byte[] block1 = new byte[8], block2 = new byte[8];
+        for(int i = 0, len = src.length / 8; i < len; i++) {
+            if(encrypting) {
+                // 加密时，先异或，再加密
+                System.arraycopy(src, i * 8, block, 0, 8);
+                for(int j = 0; j < 8; j++) {
+                    block[j] = (byte) ((block[j] ^ icvTem[j]) & 0xff);
+                }
+                desBlockFunc(key1, block, 0, block1, 0, true);
+                desBlockFunc(key2, block1, 0, block2, 0, true);
+                desBlockFunc(key3, block2, 0, result, i * 8, true);
+                System.arraycopy(result, i * 8, icvTem, 0, 8);
+            } else {
+                // 解密时，先解密，再异或
+                desBlockFunc(key1, src, i * 8, block1, 0, false);
+                desBlockFunc(key2, block1, 0, block2, 0, false);
+                desBlockFunc(key3, block2, 0, block, 0, false);
                 for(int j = 0; j < 8; j++) {
                     block[j] = (byte) ((block[j] ^ icvTem[j]) & 0xff);
                 }
--- src/test/java/com/itlgl/cryptoutil/des/DesUtilTest.java
+++ src/test/java/com/itlgl/cryptoutil/des/DesUtilTest.java
@@ -31,4 +31,58 @@ public class DesUtilTest {
         byte[] result2 = DesUtil.desCbc(key, encrypResult, icv, false);
         Assert.assertArrayEquals(src, result2);
     }
+
+    @Test
+    public void testDesEdeEcb() throws DesException {
+        byte[] key = ByteUtil.fromHex("010203040506070808070605040302010102030405060708");
+        byte[] src = ByteUtil.fromHex("01020304050607080102030405060708");
+        byte[] encrypResult = ByteUtil.fromHex("74D46E87794D042274D46E87794D0422");
+
+        byte[] result1 = DesUtil.desEdeEcb(key, src, true);
+        Assert.assertArrayEquals(encrypResult, result1);
+
+        byte[] result2 = DesUtil.desEdeEcb(key, encrypResult, false);
+        Assert.assertArrayEquals(src, result2);
+    }
+
+    @Test
+    public void testDesEeeEcb() throws DesException {
+        byte[] key = ByteUtil.fromHex("010203040506070808070605040302010102030405060708");
+        byte[] src = ByteUtil.fromHex("01020304050607080102030405060708");
+        byte[] encrypResult = ByteUtil.fromHex("9BD05B6CAF497B809BD05B6CAF497B80");
+
+        byte[] result1 = DesUtil.desEeeEcb(key, src, true);
+        Assert.assertArrayEquals(encrypResult, result1);
+
+        byte[] result2 = DesUtil.desEeeEcb(key, encrypResult, false);
+        Assert.assertArrayEquals(src, result2);
+    }
+
+    @Test
+    public void testDesEdeCbc() throws DesException {
+        byte[] key = ByteUtil.fromHex("010203040506070808070605040302010102030405060708");
+        byte[] src = ByteUtil.fromHex("01020304050607080102030405060708");
+        byte[] icv = ByteUtil.fromHex("0102030405060708");
+        byte[] encrypResult = ByteUtil.fromHex("46345C8EEBDC745C2AFA8116541775BF");
+
+        byte[] result1 = DesUtil.desEdeCbc(key, src, icv, true);
+        Assert.assertArrayEquals(encrypResult, result1);
+
+        byte[] result2 = DesUtil.desEdeCbc(key, encrypResult, icv, false);
+        Assert.assertArrayEquals(src, result2);
+    }
+
+    @Test
+    public void testDesEeeCbc() throws DesException {
+        byte[] key = ByteUtil.fromHex("010203040506070808070605040302010102030405060708");
+        byte[] src = ByteUtil.fromHex("01020304050607080102030405060708");
+        byte[] icv = ByteUtil.fromHex("0102030405060708");
+        byte[] encrypResult = ByteUtil.fromHex("E72B25AB138C9B282DE4DC6CCAA68E5D");
+
+        byte[] result1 = DesUtil.desEeeCbc(key, src, icv, true);
+        Assert.assertArrayEquals(encrypResult, result1);
+
+        byte[] result2 = DesUtil.desEeeCbc(key, encrypResult, icv, false);
+        Assert.assertArrayEquals(src, result2);
+    }
 }
